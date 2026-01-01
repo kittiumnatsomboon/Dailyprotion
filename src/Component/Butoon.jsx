5458620226aef035d30a66458be435f3736ec6df
champ2539 <champ.1136938@gmail.com>
2026-01-01 11:57:23 +0700

    Save form register

--- src/Component/Butoon.jsx
+++ src/Component/Butoon.jsx
@@ -1,7 +1,7 @@
-export default function Button({ children ,name }) {
+export default function Button({ children ,name ,type }) {
     return (
         <>
-            <button name={name}  className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+            <button name={name} type={type}  className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
                 { children }
             </button>
         </>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,6 +1,31 @@
 import Button from "../Component/Butoon"
 import Datepicker from "../Component/Datepicker"
 import Inputtextcomponent from "../Component/Input_textcomponent"
+import * as Yup from 'yup';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { useState } from "react";
+
+
+const SignupSchema = Yup.object().shape({
+    fullname: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'), //
+    email: Yup.string()
+        .email('Invalid email')
+        .required('Required'), //
+    password: Yup.string()
+        .min(8, 'Password is too short - should be 8 chars minimum.')
+        .required('Required'),
+    confirmpassword: Yup.string()
+        .min(8, 'Password is too short - should be 8 chars minimum.')
+        .required('Required'),
+});
+
+
+
+
+
 export default function Register() {
     return (
         <>
@@ -19,37 +44,82 @@ export default function Register() {
                     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <div></div>
                         <div className="p-8">
-                            <div className="space-y-4">
-                                <div className="pb-2">
-                                    <p className="text-white">ชื่อ-นามสกุล:</p>
-                                    <Inputtextcomponent name="fullname" placeholder="กรุณาระบุชื่อ-นามสกุล" type="text" />
-                                </div>
-                                <div className="pb-2">
-                                    <p className="text-white">วันเดือนปีเกิด :</p>
-                                    <Datepicker name="" />
-                                </div>
-                                <div className="pb-2">
-                                    <p className="text-white">ที่อยู่อีเมลล์:</p>
-                                    <Inputtextcomponent name="email" placeholder="กรุณาที่อยู่อีเมลล์" type="email" />
+                            <Formik
+                                initialValues={{ fullname: '', email: '', password: '' }}
+                                validationSchema={SignupSchema} // Pass the schema here
+                                onSubmit={(values, { setSubmitting }) => {
+                                    // Handle form submission
+                                    alert(JSON.stringify(values, null, 2));
+                                    setSubmitting(false);
+                                }}
+                            >
+                                {({ errors, touched }) => (
+                                    <Form>
 
-                                </div>
-                                <div className="pb-2">
-                                    <p className="text-white">รหัสผ่าน:</p>
-                                    <Inputtextcomponent name="password" placeholder="กรุณารหัสผ่าน" type="password" />
+                                        <div className="space-y-4">
+                                            <div className="pb-2">
+                                                <p className="text-white">ชื่อ-นามสกุล:</p>
+                                                <Field
+                                                    className="w-full px-4 py-2.5 text-sm
+                                        border border-gray-300 rounded-md
+                                        hover:border-indigo-400
+                                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500
+                                        transition duration-200"
+                                                    name="fullname" placeholder="กรุณาระบุชื่อ-นามสกุล" type="text" />
+                                                {errors.fullname && touched.fullname ? (
+                                                    <div className="text-red-500">{errors.fullname}</div>
+                                                ) : null}
+                                            </div>
+                                            <div className="pb-2">
+                                                <p className="text-white">วันเดือนปีเกิด :</p>
+                                                <Datepicker name="" />
+                                            </div>
+                                            <div className="pb-2">
+                                                <p className="text-white">ที่อยู่อีเมลล์:</p>
+                                                <Field
+                                                    className="w-full px-4 py-2.5 text-sm
+                                        border border-gray-300 rounded-md
+                                        hover:border-indigo-400
+                                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500
+                                        transition duration-200"
+                                                    name="email" placeholder="กรุณาที่อยู่อีเมลล์" type="email" />
 
-                                </div>
-                                <div className="pb-2">
-                                    <p className="text-white">ยืนยันรหัสผ่าน:</p>
-                                    <Inputtextcomponent name="confirmpassword" placeholder="กรุณายืนยันรหัสผ่าน" type="password" />
+                                                <ErrorMessage name="email" component="div" />
 
-                                </div>
-                                <div className="pb-2 text-center">
-                                    <Button name="btnregister">
-                                        สมัครสมาชิก
-                                    </Button>
-                                </div>
+                                            </div>
+                                            <div className="pb-2">
+                                                <p className="text-white">รหัสผ่าน:</p>
+                                                <Field
+                                                    className="w-full px-4 py-2.5 text-sm
+                                        border border-gray-300 rounded-md
+                                        hover:border-indigo-400
+                                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500
+                                        transition duration-200"
+                                                    name="password" placeholder="กรุณารหัสผ่าน" type="password" />
 
-                            </div>
+                                            </div>
+                                            <div className="pb-2">
+                                                <p className="text-white">ยืนยันรหัสผ่าน:</p>
+                                                <Field
+                                                    className="
+                                        w-full px-4 py-2.5 text-sm
+                                        border border-gray-300 rounded-md
+                                        hover:border-indigo-400
+                                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500
+                                        transition duration-200"
+                                                    name="confirmpassword" placeholder="กรุณายืนยันรหัสผ่าน" type="password" />
+
+                                            </div>
+                                            <div className="pb-2 text-center">
+                                                <Button name="btnregister" type="submit">
+                                                    สมัครสมาชิก
+                                                </Button>
+                                            </div>
+
+                                        </div>
+                                    </Form>
+                                )}
+                            </Formik>
                         </div>
                         <div></div>
                     </div>
